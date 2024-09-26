3d0dbb37d26ec635af4f5e8f5391761ee85cc048
zhaoqinghai <cayav@outlook.com>
2024-09-26 14:26:28 +0800

    init

---
+++ GetPicShell/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
+do
+{
+    var id = Guid.NewGuid().ToString();
+    using var mutex = new Mutex(true, id);
+    using var process = Process.Start(new ProcessStartInfo()
+    {
+        Arguments = id,
+        CreateNoWindow = true,
+        UseShellExecute = true,
+        FileName = "CTNewGetPic.exe",
+        WindowStyle = ProcessWindowStyle.Hidden,
+    });
+    Console.WriteLine("已启动");
+    if (process != null)
+    {
+        Console.WriteLine("等待结束");
+        await process.WaitForExitAsync();
+    }
+    Console.WriteLine("已停止");
+}
+while (await timer.WaitForNextTickAsync());
---
+++ publish.bat
@@ -0,0 +1,23 @@
+@echo off
+set targetPath=.\Publish
+
+REM 创建目标文件夹（如果不存在）
+if not exist %targetPath% (
+    mkdir %targetPath%
+)
+
+if exist %targetPath% (
+    rd /s /q %targetPath%
+)
+
+REM 发布 CTNewGetPic 到目标文件夹
+dotnet publish .\CTNewGetPic\CTNewGetPic.csproj --self-contained -r win-x64 -c Release -o %targetPath%
+
+REM 发布 ProjectB 到目标文件夹
+dotnet publish .\GetPicShell\GetPicShell.csproj -r win-x64 -c Release -o %targetPath%
+
+echo.|dotNET_Reactor -project "CTNewGetPic.nrproj"
+
+xcopy obfuscators\* Publish /s /e /y
+
+echo 发布完成，所有文件已复制到 %targetPath%
